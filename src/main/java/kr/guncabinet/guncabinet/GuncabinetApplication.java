846be9de954a79150b21a3cba342228290fe1bab
Ryniu21 <krzysiek.rutkowski89@gmail.com>
2021-07-11 17:05:11 +0200

    init commit

---
+++ src/main/java/kr/guncabinet/guncabinet/GuncabinetApplication.java
@@ -0,0 +1,13 @@
+package kr.guncabinet.guncabinet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuncabinetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuncabinetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/guncabinet/guncabinet/GuncabinetApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.guncabinet.guncabinet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuncabinetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
